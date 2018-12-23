2f041bd34ed6ca8b595713d1183952b014f5af8a
nhefner12 <45905445+nhefner12@users.noreply.github.com>
2018-12-23 15:20:19 -0800

    Initial CNBC scrapper layout
    Decided to be set up view and controller to parse from CNBC website.
    base application page and CNBC page created.

--- MVC_WebScrapper/MVC_WebScrapper/Controllers/CNBCController.cs
+++ MVC_WebScrapper/MVC_WebScrapper/Controllers/CNBCController.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace MVC_WebScrapper.Controllers
 {
-    public class HomeController : Controller
+    public class CNBCController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
---
+++ MVC_WebScrapper/MVC_WebScrapper/Models/CNBCModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_WebScrapper.Models
+{
+    public class CNBCModel
+    {
+        
+        public string Headline { get; set; }
+        public string Img { get; set; }
+        public string Detail { get; set; }
+
+    }
+
+    public class CNBCStories
+    {
+        public string Type { get; set; }
+        public List<CNBCModel> stories { get; set; }
+    }
+}
